7b5ccc4da4d3ba3e2c9e3905d886d74d832112d2
Kristian Epps <kristian@xepps.com>
2017-12-02 10:14:14 +0000

    Day 2, part 1

---
+++ Day-2/solution.js
@@ -0,0 +1,16 @@
+const determine = (input) => 
+    input
+        .split('\n')
+        .map(line => line
+            .split(/\s+/)
+            .map((i) => parseInt(i, 10))
+            .sort()
+            .reverse()
+            .filter((e, i, a) => i === 0 || i === a.length -1)
+            .reduce((acc, e) => acc - e)
+        )
+        .reduce((acc, e) => acc + e)
+
+module.exports = {
+    part1: (input) => determine(input)
+};
---
+++ Day-2/test.js
@@ -0,0 +1,23 @@
+const readFile = require('fs').readFileSync; 
+const join = require('path').join;
+
+const { part1, part2 } = require('./solution');
+
+describe.only('day 1', () => {
+    describe('part 1', () => {
+        test('Sample spreadsheet gives the checksum 18', () => {
+            const input = `5   1   9   5
+7   5   3
+2   4   6   8`;
+            expect(part1(input)).toBe(18);
+        });
+       
+        describe('What is the checksum for the input sheet?', () => {
+            test(9804, () => {
+                const input = readFile(join(__dirname, 'input.txt'), 'utf-8');
+            
+                expect(part1(input)).toBe(9804);
+            });
+        });
+    });
+});
