3b55cb7181257773991ce4cfa141e251dc69a23d
Kristian Epps <kristian@xepps.com>
2017-12-08 12:24:17 +0000

    day 7 part 1

--- Day-7/solution.js
+++ Day-7/solution.js
@@ -1,23 +1,30 @@
-const formatInput = input => {
+const copy = o => JSON.parse(JSON.stringify(o));
+
+const buildTree = input => {
     const rows = input.split('\n');
     let output = {};
     rows.forEach((row) => {
         const parsed = /([a-z]+) \(([0-9]+)\)( -> )?([a-z, ]*)/.exec(row);
         output[parsed[1]] = { 
+            name: parsed[1],
             weight: parsed[2], 
-            branches: parsed[3] ? parsed[4].split(', ') : null 
+            branches: parsed[3] ? parsed[4].split(', ') : [] 
         };
     });
 
+    var allBranches = [];
+    Object.keys(output).forEach((node) => {
+        var branches = copy(output[node].branches);
+        output[node].branches = branches.map((branch) => copy(output[branch]));
+        allBranches = allBranches.concat(branches);
+    });
+    allBranches.forEach(branch => delete output[branch]);
+    
     return output;
 
 }
 
-const getTreeBase = () => {
-
-}
-
 module.exports = {
-    part1: (input) => formatInput(input),
+    part1: (input) => Object.keys(buildTree(input))[0],
     part2: (input) => null
 };
