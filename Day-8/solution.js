0c6ea25d68a03b1950a43bc8e3535a6a31eb5042
Kristian Epps <kristian@xepps.com>
2017-12-08 17:35:53 +0000

    Touch and go on day 8

--- Day-8/solution.js
+++ Day-8/solution.js
@@ -1,5 +1,34 @@
+const toObj = row => {
+    return {r: row[1], m: row[2], by: row[3], exp: row[4], or: row[5]};
+}
+
+const toCode = ({r, m, by, exp, or}) => {
+    return `
+        if(r.${r} === undefined) r.${r} = 0;
+        if(r.${or} === undefined) r.${or} = 0;
+        if(r.${exp}) r.${r} ${m === 'inc' ? '+=' : '-='} ${by};
+        if(r.${r} > h) h = r.${r};
+`
+}
+
+const genCode = input => 
+    `(function() {
+        let r = {};
+        let h = 0; ${
+        input.split('\n')
+            .map((row) => /([a-z]+) (inc|dec) ([0-9-]+) if (([a-z]+) .*)/.exec(row))
+            .map(toObj)
+            .map(toCode)
+            .join(' ')
+        }
+        return {regs: r, highest: h};
+    })();`;
+
 module.exports = {
-    // ([a-z]+) (inc|dec) ([0-9-]+) if (([a-z])+ .*)
-    part1: (input) => null,
-    part2: (input) => null
+    part1: (input) => {
+        const regs = eval(genCode(input)).regs;
+
+        return Object.keys(regs).map(key => regs[key]).sort((a,b) => b - a)[0]
+    },
+    part2: (input) => eval(genCode(input)).highest
 };
--- Day-8/test.js
+++ Day-8/test.js
@@ -3,7 +3,32 @@ const join = require('path').join;
 
 const { part1, part2 } = require('./solution');
 
-describe.only('day 8', () => {
+describe('day 8', () => {
     describe('part 1', () => {
+        test('After this process, the largest value in any register is 1.', () => {
+            const input = readFile(join(__dirname, 'sample.txt'), 'utf-8');
+
+            expect(part1(input)).toBe(1);
+        });
+
+        test('What is the largest value in any register after completing the instructions in your puzzle input?', () => {
+            const input = readFile(join(__dirname, 'input.txt'), 'utf-8');
+
+            expect(part1(input)).toBe(5966);
+        });
+    });
+
+    describe('part 2', () => {
+        test('in the above instructions, the highest value ever held was 10', () => {
+            const input = readFile(join(__dirname, 'sample.txt'), 'utf-8');
+
+            expect(part2(input)).toBe(10);
+        });
+
+        test('The CPU also needs to know the highest value held in any register during this process/', () => {
+            const input = readFile(join(__dirname, 'input.txt'), 'utf-8');
+
+            expect(part2(input)).toBe(6347);
+        });
     });
 });
