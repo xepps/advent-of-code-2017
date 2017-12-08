2a4c1b932f0d623229d33c94963d9aed33373b17
Kristian Epps <kristian@xepps.com>
2017-12-08 13:29:42 +0000

    start day 8

---
+++ Day-8/solution.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // ([a-z]+) (inc|dec) ([0-9-]+) if (([a-z])+ .*)
+    part1: (input) => null,
+    part2: (input) => null
+};
---
+++ Day-8/test.js
@@ -0,0 +1,9 @@
+const readFile = require('fs').readFileSync; 
+const join = require('path').join;
+
+const { part1, part2 } = require('./solution');
+
+describe.only('day 8', () => {
+    describe('part 1', () => {
+    });
+});
