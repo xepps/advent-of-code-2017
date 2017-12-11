e288bdf8f2d456a3fb192438fc2daae4251cff50
Kristian Epps <kristian@xepps.com>
2017-12-11 14:45:27 +0000

    set up for day 11

---
+++ Day-11/solution.js
@@ -0,0 +1,4 @@
+module.exports = {
+    part1: (input) => null,
+    part2: (input) => null
+};
---
+++ Day-11/test.js
@@ -0,0 +1,25 @@
+const readFile = require('fs').readFileSync; 
+const join = require('path').join;
+
+const { part1, part2 } = require('./solution');
+
+describe('day 11', () => {
+    describe('part 1', () => {
+        [
+            { path: 'ne,ne,ne', distance: 3 },
+            { path: 'ne,ne,sw,sw', distance: 0 },
+            { path: 'ne,ne,s,s', distance: 2 },
+            { path: 'se,sw,se,sw,sw', distance: 3 }
+        ].forEach(({path, distance}) => {
+            it(`should take ${distance} steps to return from going ${path}`, () => {
+                expect(part1(path)).toBe(distance);
+            });
+        });
+
+        test('How many steps does it take to return from the input\'s path?', () => {
+            const path = readFile(join(__dirname, 'input.txt'), 'utf-8');
+            
+            expect(part1(path)).toBe('???');
+        });
+    });
+});
